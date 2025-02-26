706143bec4fdbc2ecb147130b257daf1903d72c8
Iker Saiz Martinez <iksaiz-m@c4r8s2.42urduliz.com>
2025-02-26 17:36:41 +0100

    exit.c cambios

--- env_funciona/exit.c
+++ env_funciona/exit.c
@@ -6,7 +6,7 @@
 /*   By: iksaiz-m <iksaiz-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 20:48:06 by iksaiz-m          #+#    #+#             */
-/*   Updated: 2025/02/26 17:31:00 by iksaiz-m         ###   ########.fr       */
+/*   Updated: 2025/02/26 17:36:21 by iksaiz-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,11 @@
 
 int	ft_exit(t_mini *data)
 {
-	int	i;
-	
-	i = 0;
-	// while (data->commands[i])
-	// {
-	// 	printf("%i: %s\n", i, data->commands[i]);
-	// 	i++;
-	// }
 	if (data->splits <= 2)
 	{
 		if (ft_strncmp(data->commands[0], "exit", 4) == 0
 			&& !data->commands[0][4])
 		{
-			// printf("free %p\n", data->commands);
 			free_split(data->commands);
 			printf("\033[31mFin.\033[0m\n");
 			free(data);
